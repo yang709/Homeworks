a662ffa558e609677501948798cc50c8cb2607fd
Xuan Yang <yang709@purdue.edu>
2018-09-21 13:33:15 +0800

    w

--- CS252/Project6/testapp/server.js
+++ CS252/Project6/testapp/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 
 io.on('connection', function(socket){
@@ -49,7 +49,7 @@ app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
 /**
